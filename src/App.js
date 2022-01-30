ef613bc8031210775a0737c1b4d63f9123dd5831
Praneeth Karnatakam <66370487+praneethk237@users.noreply.github.com>
2022-01-30 23:14:40 +0530

    space

--- src/App.js
+++ src/App.js
@@ -313,7 +313,7 @@ function App() {
           <Extras type="Soups" items={soups}/>
           <Extras type="Papad's" items={papads} />
           
-          <Extras type="ChineseDelights" items={chineseDelights} />
+          <Extras type="Chinese Delights" items={chineseDelights} />
           <Extras type="Starters" items={emptyItems} />
           <ExtrasWithoutType subtype="Gobi" items={GobiStarters} ></ExtrasWithoutType>
           <ExtrasWithoutType subtype="Panner" items={PannerStarters} ></ExtrasWithoutType>
